0b4686a25be99e4e30e78bce08be4d2941c53023
quentenl <qlundie@gmail.com>
2019-09-22 11:32:13 -0400

    updated comments

--- Lab 3/exercise12.py
+++ Lab 3/exercise12.py
@@ -6,4 +6,5 @@ degrees_f = int(input("What is the temperature in Farenheit? "))
 # formula to convert Farenheit to Celsius is: (degrees farenheit minus 32) multiplied by (5/9) 
 degrees_c = (degrees_f-32) * (5 / 9) 
 
+# display the converted temperature to the user
 print(degrees_f, "in degrees Farenheit is", degrees_c, " degrees in Celsius.")
